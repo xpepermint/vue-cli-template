109836a87a320026e1aebc2c4b656e1923042d8c
Tadej Stanic <stanic.tadej@gmail.com>
2016-11-23 15:57:30 +0100

    Fix css hot reload

--- template/src/app/client-entry.js
+++ template/src/app/client-entry.js
@@ -1,3 +1,16 @@
 import {app} from '.';
 
 app.$mount('#app');
+
+// When Webpack builds css, this code will ensure that new styles will be shown in the browser.
+if (module.hot) {
+  const reporter = window.__webpack_hot_middleware_reporter__;
+  const success = reporter.success;
+  reporter.success = function () {
+    document.querySelectorAll('link[href][rel=stylesheet]').forEach((link) => {
+      const nextStyleHref = link.href.replace(/(\?\d+)?$/, `?${Date.now()}`);
+      link.href = nextStyleHref;
+    });
+    success();
+  };
+}
